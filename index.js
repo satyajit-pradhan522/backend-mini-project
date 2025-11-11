2523a8c4713991c5245803373b9126817df0ec8c
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-11-12 02:45:01 +0530

    projects complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -9,7 +10,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+    fs.readdir(`./files`, (err, files) => {
+        res.render("index", { files: files });
+    });
+});
+
+app.get('/files/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+        res.render("show", { title: req.params.filename.split('_').join(' ').replace('.txt', ''), details: data });
+    });
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('_')}.txt`, req.body.details, (err) => {
+        res.redirect('/');
+    });
 });
 
 app.listen(port, () => {
