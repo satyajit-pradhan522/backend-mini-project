ef292e88bfde9a2a683c971dff6a6adf9cb616a9
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-11-12 18:55:41 +0530

    complete

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    fs.readdir(`./files`, (err, files) => {
-        res.render("index", { files: files });
-    });
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
-app.post('/create', (req, res) => {
-    fs.writeFile(`./files/${req.body.title.split(' ').join('_')}.txt`, req.body.details, (err) => {
-        res.redirect('/');
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.render("show", {
+      title: req.params.filename.split("_").join(" ").replace(".txt", ""),
+      details: data,
     });
+  });
 });
 
-app.get('/files/:filename', (req, res) => {
-    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(404).send('File not found');
-        }
-        res.render("show", { title: req.params.filename.split('_').join(' ').replace('.txt', ''), details: data });
-    });
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
 });
 
+app.post("/edit", (req, res) => {
+  const oldName = `./files/${req.body.previousTitle}`;
+  const newName = `./files/${req.body.newTitle.split(' ').join('_')}.txt`;
+  fs.rename(
+    oldName,
+    newName,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
 
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("_")}.txt`,
+    req.body.details,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error renaming file");
+      }
+      res.redirect("/");
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-}); 
+});
